3db4ba49bac02cd4020be15c125b896ed7e4f60a
hideki-yoshioka <syazaku73@hotmail.com>
2020-01-21 11:28:40 +0900

    公開

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # sourceにpush
+echo 'git push origin source'
 git push origin source
 
 # docsディレクトリに公開データをoutput
+echo 'hugo'
 hugo;
 
 files=`git status|grep -e "modified" -e "new file"`;
 
 if [ -n "$files" ]; then
     # 差分データがある
+    echo 'git commit -m "公開"'
     git add -A;
-    git add commit -m '公開';
+    git commit -m '公開';
     # sourceにpush
+    echo 'git push origin source'
     git push origin source
     # masterにpush
+    echo 'git subtree push --prefix docs/ origin master'
     git subtree push --prefix docs/ origin master
 else
     # 差分データがない
